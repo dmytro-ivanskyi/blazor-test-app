458269af96bd5be2a7589c18cb3ebf5d420b970e
Dmytro Ivanskyi <ivanskoy.d@gmail.com>
2021-11-01 12:41:35 +0200

    Created input field

---
+++ BlazorTestApp/NameModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTestApp
+{
+    public class NameModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "You nead at least 3 letters!")]
+        public string Name { get; set; }
+    }
+}
