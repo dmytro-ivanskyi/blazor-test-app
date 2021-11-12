5753992a41fdd97dbb713a393623ed4334219b5e
Dmytro Ivanskyi <ivanskoy.d@gmail.com>
2021-11-12 11:19:05 +0200

    Renamed test file

---
+++ PlaywriteUITests/IndexPageTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PlaywrightUITests
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class Tests
+    {
+        private IBrowser _browser;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 3000
+            });
+            _browser = browser;
+        }
+
+        [Test]
+        public async Task IndexPageTitle_IsCorrectAsync()
+        {
+            
+
+            var page = await _browser.NewPageAsync();
+
+            await page.GotoAsync("http://localhost:5000/");
+            var title = await page.QuerySelectorAsync("title");
+
+            Assert.AreEqual("BlazorTestApp", await title.InnerTextAsync());
+        }
+
+
+        [Test]
+        public async Task IndexPage_ClickSubmitWithCheckbox_IsValid()
+        {
+
+            var page = await _browser.NewPageAsync();
+
+            await page.GotoAsync("http://localhost:5000/");
+            var title = await page.QuerySelectorAsync("title");
+
+            Assert.AreEqual("BlazorTestApp", await title.InnerTextAsync());
+        }
+    }
+}
